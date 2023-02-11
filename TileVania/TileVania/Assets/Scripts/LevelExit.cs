6dd5d4118fa2b6d81604ddb77a68275825f0376f
Gulyás Péter <gulyaspeter20@gmail.com>
2023-02-11 16:36:47 +0100

    TileVania

---
+++ TileVania/TileVania/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float levelLoadDelay = 5f;
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.tag == "Player")
+            StartCoroutine(LoadLevel());
+    }
+    IEnumerator LoadLevel()
+    {
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if(nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+            nextSceneIndex = 0;
+        SceneManager.LoadScene(nextSceneIndex);
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+    }
+}
--- TileVania/TileVania/Assets/Scripts/PlayerMovementController.cs
+++ TileVania/TileVania/Assets/Scripts/PlayerMovementController.cs
@@ -8,24 +8,32 @@ public class PlayerMovementController : MonoBehaviour
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float jumpVelocity = 10f;
     [SerializeField] float climbSpeed = 10f;
+    [SerializeField] Vector2 deathKick = new Vector2(10f, 10f);
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform gun;
     Vector2 moveInput;
-    Rigidbody2D rigidbody2D;
+    Rigidbody2D playerRigidbody2D;
     Animator playerAnimator;
-    CapsuleCollider2D playerCapsuleCollider2D;
+    CapsuleCollider2D playerBodyCapsuleCollider2D;
+    BoxCollider2D playerFeetBoxCollider2D;
     float gravityScale;
+    bool isAlive = true;
 
     void Start()
     {
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        playerRigidbody2D = GetComponent<Rigidbody2D>();
         playerAnimator = GetComponent<Animator>();
-        playerCapsuleCollider2D = GetComponent<CapsuleCollider2D>();
-        gravityScale = rigidbody2D.gravityScale;
+        playerBodyCapsuleCollider2D = GetComponent<CapsuleCollider2D>();
+        playerFeetBoxCollider2D = GetComponent<BoxCollider2D>();
+        gravityScale = playerRigidbody2D.gravityScale;
     }
     void Update()
     {
+        if(!isAlive)return;
         Run();
         FlipSprite();
         ClimbLadder();
+        Die();
     }
     void OnMove(InputValue value)
     {
@@ -33,42 +41,59 @@ public class PlayerMovementController : MonoBehaviour
     }
     void OnJump(InputValue value)
     {
-        if(!playerCapsuleCollider2D.IsTouchingLayers
+        if(!isAlive)return;
+        if(!playerFeetBoxCollider2D.IsTouchingLayers
             (LayerMask.GetMask("Ground"))){return;}
         if(value.isPressed)
         {
-            rigidbody2D.velocity += new Vector2(0f, jumpVelocity);
+            playerRigidbody2D.velocity += new Vector2(0f, jumpVelocity);
         }
+    }
+    void OnFire(InputValue value)
+    {
+         if(!isAlive)return;
+        Instantiate(bullet, gun.position, transform.rotation);
     }
     void Run()
     {
-        Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, rigidbody2D.velocity.y);
-        rigidbody2D.velocity = playerVelocity;
-        bool playerHasHorizontalSpeed = Mathf.Abs(rigidbody2D.velocity.x) > Mathf.Epsilon;
+        Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, playerRigidbody2D.velocity.y);
+        playerRigidbody2D.velocity = playerVelocity;
+        bool playerHasHorizontalSpeed = Mathf.Abs(playerRigidbody2D.velocity.x) > Mathf.Epsilon;
         playerAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
     }
     void FlipSprite()
     {
-        bool playerHasHorizontalSpeed = Mathf.Abs(rigidbody2D.velocity.x) > Mathf.Epsilon;
+        bool playerHasHorizontalSpeed = Mathf.Abs(playerRigidbody2D.velocity.x) > Mathf.Epsilon;
         if(playerHasHorizontalSpeed)
         {
-            transform.localScale = new Vector2(Mathf.Sign(rigidbody2D.velocity.x), 1f);
+            transform.localScale = new Vector2(Mathf.Sign(playerRigidbody2D.velocity.x), 1f);
         }
     }
     void ClimbLadder()
     {
-        if(!playerCapsuleCollider2D.IsTouchingLayers(LayerMask.GetMask("Climb")))
+        if(!isAlive)return;
+        if(!playerFeetBoxCollider2D.IsTouchingLayers(LayerMask.GetMask("Climb")))
         {
-            rigidbody2D.gravityScale = gravityScale;
+            playerRigidbody2D.gravityScale = gravityScale;
             playerAnimator.SetBool("Climb", false);
             return;
         }
-            Vector2 climbVelocity = new Vector2(rigidbody2D.velocity.x, moveInput.y * climbSpeed);
-            rigidbody2D.velocity = climbVelocity;
-            rigidbody2D.gravityScale = 0f;
+            Vector2 climbVelocity = new Vector2(playerRigidbody2D.velocity.x, moveInput.y * climbSpeed);
+            playerRigidbody2D.velocity = climbVelocity;
+            playerRigidbody2D.gravityScale = 0f;
 
-            bool playerHasVerticalSpeed = Mathf.Abs(rigidbody2D.velocity.y) > Mathf.Epsilon;
+            bool playerHasVerticalSpeed = Mathf.Abs(playerRigidbody2D.velocity.y) > Mathf.Epsilon;
             playerAnimator.SetBool("Climb", playerHasVerticalSpeed);
-        
+    }
+    void Die() {
+        {
+            if(playerBodyCapsuleCollider2D.IsTouchingLayers(LayerMask.GetMask("Enemies", "Hazards")))
+            {
+                isAlive = false;
+                playerAnimator.SetTrigger("Dying");
+                playerRigidbody2D.velocity = deathKick;
+                FindObjectOfType<GameSession>().ProcessPlayerDeath();
+            }
+        }
     }
 }
