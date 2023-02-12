b0642f8de9c8b282008c4034d6834f5a09f77100
Gulyás Péter <gulyaspeter20@gmail.com>
2023-02-12 20:02:07 +0100

    Laser Defender
    Looping enemy spawning.

--- Laser Defender/Assets/Scripts/EnemySpawner.cs
+++ Laser Defender/Assets/Scripts/EnemySpawner.cs
@@ -7,7 +7,9 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] List<WaveConfigSO> waveConfigs;
     [SerializeField] float timeBetweenWaves = 0f;
+    [SerializeField] bool isLooping = true;
     WaveConfigSO currentWave;
+    
     void Start()
     {
         StartCoroutine(SpawnEnemyWaves());
@@ -15,19 +17,21 @@ public class EnemySpawner : MonoBehaviour
 
     IEnumerator SpawnEnemyWaves()
     {
-        foreach(WaveConfigSO wave in waveConfigs)
-        {
-            currentWave = wave;
-            for(int i = 0; i < currentWave.GetEnemyCount(); i++)
+        do{
+            foreach(WaveConfigSO wave in waveConfigs)
             {
-                Instantiate(currentWave.GetEnemyPrefab(i), 
-                            currentWave.GetStartingWayPoint().position, 
-                            Quaternion.identity, 
-                            transform);
-                yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+                currentWave = wave;
+                for(int i = 0; i < currentWave.GetEnemyCount(); i++)
+                {
+                    Instantiate(currentWave.GetEnemyPrefab(i), 
+                                currentWave.GetStartingWayPoint().position, 
+                                Quaternion.identity, 
+                                transform);
+                    yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+                }
+                yield return new WaitForSeconds(timeBetweenWaves);
             }
-            yield return new WaitForSeconds(timeBetweenWaves);
-        }
+        }while(isLooping)
     }
     public WaveConfigSO GetCurrentWave()
     {
